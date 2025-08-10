75bf412fe2d14327ed9940a76aa29cd007762cca
Mar <dar.marinich@gmail.com>
2025-08-10 21:11:16 +0500

    initial commit

---
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static java.time.LocalDate.now;
+
+
+public class RegistrationTest {
+
+    public String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+    @Test
+
+    void shouldRegisterAplicationCard() {
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+
+        Selenide.open("http:localhost:9999");
+        $("[data-test-id='city'] input").setValue("Екатеринбург");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").press(Keys.DELETE);
+        $("[data-test-id='date'] input"). setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Петров Сергей");
+        $("[data-test-id='phone'] input").setValue("+71231231231");
+        $(".checkbox__text").click();
+        $(".button__text").click();
+        $(Selectors.withText("Встреча успешно забронирована")).should(Condition.visible,
+                Duration.ofSeconds(15));
+
+    }
+
+
+}
